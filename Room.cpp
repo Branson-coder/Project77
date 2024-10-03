1b94e8923483b2e215920587c38fdae12868ca9b
Branson Khoo <a1925025@adelaide.edu.au>
2024-10-03 14:29:38 +0930

    Room

--- Room.cpp
+++ Room.cpp
@@ -1,51 +1,36 @@
 #include <vector>
+#include <SFML/Graphics.hpp>
 #include "Room.h"
 #include "Wall.h"
+
 using namespace std;
 
 Room:: Room(sf::Texture* texture, sf::Vector2f position, int roomType){
 	
+    if (roomType == 1) {
+        for (int x = 0; x < maxSize.x; x++) {
+            layout.push_back(vector<Wall>());
+            for (int y = 0; y < maxSize.y; y++) {
+                
+                if (x == 0 || x == maxSize.x - 1 || y == 0 || y == maxSize.y - 1 || y == 0){
+                   
+                    layout[x].push_back(Wall(texture, sf::Vector2f(100.0f, 100.f), position + sf::Vector2f(100.0f * x, 100.0f * y), sf::Color::White, true));
 
- if (roomType == 1)
-	{
-		for (int x = 0; x < maxSize.x; x++)
-		{
-			layout.push_back(vector<Wall>());
-			for (int y = 0; y < maxSize.y; y++)
-			{
-				if (x == 0 || x == maxSize.x - 1)
-				{
-					if (y != 5)
-					{
-						layout[x].push_back(Wall(texture, sf::Vector2f(100.0f, 100.f), position + sf::Vector2f(100.0f * x, 100.0f * y), sf::Color:: White, true));
-					}
-					else
-					{
-						layout[x].push_back(Wall(nullptr, sf::Vector2f(100.0f, 100.f), position + sf::Vector2f(100.0f * x, 100.0f * y), sf::Color:: Transparent, false));
-					}
 
-				}
-				else if (y == 0 || y == maxSize.y - 1)
-				{
-					if (x != 8)
-					{
-						layout[x].push_back(Wall(texture, sf::Vector2f(100.0f, 100.f), position + sf::Vector2f(100.0f * x, 100.0f * y), sf::Color:: White, true));
-					}
-					else
-					{
-						layout[x].push_back(Wall(nullptr, sf::Vector2f(100.0f, 100.f), position + sf::Vector2f(100.0f * x, 100.0f * y), sf::Color:: Transparent, false));
-					}
-				}
-				else
-				{
-					layout[x].push_back(Wall(nullptr, sf::Vector2f(100.0f, 100.f), position + sf::Vector2f(100.0f * x, 100.0f * y), sf::Color:: Transparent, false));
-				}
+                } else {
+                
+                    layout[x].push_back(Wall(nullptr, sf::Vector2f(100.0f, 100.f), position + sf::Vector2f(100.0f * x, 100.0f * y), sf::Color::Transparent, false));
+					
+                }
 
-			}
-		}
-	}
-
+				
+            }
+        }
+    }
 }
+
+ 
+
 
 void Room::Display(sf::RenderWindow & window, Collider playerCollider, Player &player){
 	
@@ -66,3 +51,4 @@ void Room::Display(sf::RenderWindow & window, Collider playerCollider, Player &p
 	}
 	
 }
+
