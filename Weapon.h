7b60cca714a9860aa66077f98dc9f71e97103b22
Branson Khoo <a1925025@adelaide.edu.au>
2024-10-03 14:16:33 +0930

    testing

--- Weapon.h
+++ Weapon.h
@@ -1,5 +1,10 @@
 #ifndef WEAPON_H
 #define WEAPON_H
-
+#include <SFML/Graphics.hpp>
+#include <iostream>
+using namespace std;
+class Weapon{
+Weapon(sf::Texture *texutre, sf::Vector2f size, float damage, float );
+};
 
 #endif
